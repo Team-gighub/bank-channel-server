bfa85f12be2bf93eb5c2cded573e1855aaeb51f8
ye0ngeun <duddms2415@naver.com>
2025-11-19 15:40:53 +0900

    setting: 초기 프로젝트 구조 세팅

---
+++ src/main/java/com/bank/channel/BankChannelApplication.java
@@ -0,0 +1,13 @@
+package com.bank.channel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankChannelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankChannelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/channel/BankChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.channel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankChannelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
