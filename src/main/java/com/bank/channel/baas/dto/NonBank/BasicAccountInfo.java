03d159e31991f9f4da0462ddec0969a3e95ba67c
yuunseo <dbstj7457@naver.com>
2025-11-29 11:48:29 +0900

    feat: BasicAccountInfo에 @AllArgsConstructor 추가 (final field 초기화 문제 해결)

--- src/main/java/com/bank/channel/baas/dto/NonBank/BasicAccountInfo.java
+++ src/main/java/com/bank/channel/baas/dto/NonBank/BasicAccountInfo.java
@@ -1,10 +1,12 @@
 package com.bank.channel.baas.dto.NonBank;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class BasicAccountInfo {
     private final String accountNo;
     private final String bankCode;
