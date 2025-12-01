61a674c973c5b8f03a187281b6020496c1ef8b13
yuunseo <dbstj7457@naver.com>
2025-12-01 15:43:59 +0900

    fix: build 오류 해결 (serivce null 주입)

--- src/main/java/com/bank/channel/baas/dto/NonBank/AccountInfoWithPhone.java
+++ src/main/java/com/bank/channel/baas/dto/NonBank/AccountInfoWithPhone.java
@@ -1,10 +1,14 @@
 package com.bank.channel.baas.dto.NonBank;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
 public class AccountInfoWithPhone extends BasicAccountInfo {
-    private final String phone;
+    private String phone;
 }
