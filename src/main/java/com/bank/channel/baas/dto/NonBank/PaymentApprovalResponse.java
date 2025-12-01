643b8831baecb8d05b0d423246cd5884f9e263d5
yuunseo <dbstj7457@naver.com>
2025-12-01 16:21:14 +0900

    refactor: DTO 내 필드 변경

--- src/main/java/com/bank/channel/baas/dto/NonBank/PaymentApprovalResponse.java
+++ src/main/java/com/bank/channel/baas/dto/NonBank/PaymentApprovalResponse.java
@@ -3,6 +3,8 @@ package com.bank.channel.baas.dto.NonBank;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.math.BigDecimal;
+
 /**
  * [결제 승인] API (/payment/approval) 응답 DTO
  */
@@ -10,4 +12,8 @@ import lombok.Getter;
 @Builder
 public class PaymentApprovalResponse {
     private final String escrowId; // 에스크로 결제 승인 TID
+    private final BigDecimal amount; //대금
+    private final String status; //대금예치 상태
+    private final String createdAt; //결제일
+    private final String releaseDate; //지급확정일(예정일)
 }
--- src/main/java/com/bank/channel/baas/dto/NonBank/PaymentConfirmRequest.java
+++ src/main/java/com/bank/channel/baas/dto/NonBank/PaymentConfirmRequest.java
@@ -10,8 +10,6 @@ import lombok.Getter;
 @Builder
 public class PaymentConfirmRequest {
     private final String merchantId; // 고객사 아이디
-    private final String reqYmd; // 요청일자 ( YYYY-MM-DD )
     private final String escrowId; // 거래번호 ( 에스크로 결제 승인 TID )
-    private final String changerId; // 처리자 아이디
 
 }
