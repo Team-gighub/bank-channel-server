4f23bb44ea57afc3cf0181b1cf44cc071fbf16ef
yuunseo <dbstj7457@naver.com>
2025-12-01 16:33:45 +0900

    refactor: 결제 승인 DTO 변경

---
+++ src/main/java/com/bank/channel/baas/domain/enums/HoldStatus.java
@@ -0,0 +1,5 @@
+package com.bank.channel.baas.domain.enums;
+
+public enum HoldStatus {
+    ACTIVE, RELEASED, CANCELLED
+}
--- src/main/java/com/bank/channel/baas/dto/NonBank/PaymentApprovalResponse.java
+++ src/main/java/com/bank/channel/baas/dto/NonBank/PaymentApprovalResponse.java
@@ -1,9 +1,11 @@
 package com.bank.channel.baas.dto.NonBank;
 
+import com.bank.channel.baas.domain.enums.HoldStatus;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 /**
  * [결제 승인] API (/payment/approval) 응답 DTO
@@ -12,8 +14,8 @@ import java.math.BigDecimal;
 @Builder
 public class PaymentApprovalResponse {
     private final String escrowId; // 에스크로 결제 승인 TID
-    private final BigDecimal amount; //대금
-    private final String status; //대금예치 상태
-    private final String createdAt; //결제일
-    private final String releaseDate; //지급확정일(예정일)
+    private final BigDecimal holdAmount;
+    private final HoldStatus holdStatus;
+    private final BigDecimal platformFee;
+    private final LocalDateTime holdStartDatetime;
 }
