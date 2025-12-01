c88e4d912a21837cc05aee78a06ea692b22875d6
yuunseo <dbstj7457@naver.com>
2025-12-01 15:52:22 +0900

    fix: final 유지를 위한 기본 생성자 어노테이션

--- src/main/java/com/bank/channel/baas/dto/NonBank/BasicAccountInfo.java
+++ src/main/java/com/bank/channel/baas/dto/NonBank/BasicAccountInfo.java
@@ -2,11 +2,13 @@ package com.bank.channel.baas.dto.NonBank;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
 @AllArgsConstructor
+@NoArgsConstructor(force = true)
 public class BasicAccountInfo {
     private final String accountNo;
     private final String bankCode;
